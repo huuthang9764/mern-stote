3659ab725b70ccb33a53c1d8d705fa2d1995c9e1
Nguyễn Hữu Thắng <76506824+huuthang9764@users.noreply.github.com>
2023-03-16 12:36:10 +0700

    1603

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Stotezona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
